5e6168684224299b2e7ab1c4204bc4c0d0852bce
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-22 23:45:11 +0100

    created an event and pass props among each component[prop drilling]

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+  }
+
   return (
     <>
       {/*Header UI and Add a prop to the header for dynamic use */}
@@ -12,7 +19,7 @@ function App() {
       {/* Container of the body */}
       <div className="container">
         {/* Feedback Item UI */}
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({feedback}) {
-  if(!feedback || feedback.length === 0) {
-    return <p>No Feedback Yes</p>
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>
   }
-  return <div className='feedback-list'>
-    {feedback.map(item => <FeedbackItem key={item.id} item={item} />)}
-  </div>
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  )
 }
 
 FeedbackList.prototype = {
