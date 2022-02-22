4e6da934d7c91f3f4bef14c0bf28ceb257996a02
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-22 09:30:09 +0100

    created a FeedbackItem component and added component level state in it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
 function App() {
   return (
     <>
       {/*Header UI and Add a prop to the header for dynamic use */}
-      <Header logo='Feedback UI'/>
+      <Header logo="Feedback UI" />
       {/* Container of the body */}
       <div className="container">
-        <h1>My App</h1>
+        {/* Feedback Item UI */}
+        <FeedbackItem />
       </div>
     </>
   )
