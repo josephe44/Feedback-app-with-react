08a21c4e86c62b0d51b1ec210b7d9710714ee718
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-22 09:53:12 +0100

    managing global state in all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       {/*Header UI and Add a prop to the header for dynamic use */}
@@ -9,7 +12,7 @@ function App() {
       {/* Container of the body */}
       <div className="container">
         {/* Feedback Item UI */}
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   )
