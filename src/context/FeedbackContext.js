01c42ea7d5c73978fd200bde5566f6c3b4583b27
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-26 13:35:02 +0100

    create a mockup json server backend and fetch the feedback from it

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,20 +1,29 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    },
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchfeedbackData()
+  }, [])
+
+  //fetch data from backend
+  const fetchfeedbackData = async () => {
+    const response = await fetch(
+      'http://localhost:5000/feedback?_sort=id&_order=desc'
+    )
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Delete a feedback
   const deleteFeedback = (id) => {
@@ -50,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
         // Piece of state for feedback and feedbackEdit
         feedback,
         feedbackEdit,
+        isLoading,
         // Function to delete, add and edit
         deleteFeedback,
         addFeedback,
