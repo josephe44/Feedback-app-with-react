a49c246271d56dd16fe8f1b94354d76b5a99679a
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-26 12:32:01 +0100

    created an update feature to the feedback app

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -11,24 +11,50 @@ export const FeedbackProvider = ({ children }) => {
       rating: 10,
     },
   ])
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
 
+  // Delete a feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  //Add a feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  //Edit a feedback when clicked
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
+  //Update feedback Item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    )
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
+        // Piece of state for feedback and feedbackEdit
         feedback,
+        feedbackEdit,
+        // Function to delete, add and edit
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
